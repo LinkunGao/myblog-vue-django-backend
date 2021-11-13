1890b00cb95163f906b2d4da80709aecefcb2e53
LinkunGao <gaolinkun123@gmail.com>
2021-11-13 22:21:44 +1300

    connect myaql database

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from myblog.models import SiteInfo
 
 # Register your models here.
+admin.site.register(SiteInfo)
--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SiteInfo(models.Model):
+    title = models.CharField(null=True,blank=True, max_length=50)
+    icon = models.ImageField(upload_to='logo/', null=True, blank=True)
+    
+# pip install Pillow
+    def __int__(self):
+        return self.id
