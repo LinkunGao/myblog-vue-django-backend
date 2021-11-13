c1aeae6fa3de0d7e98a502261cfc377c25883a4d
LinkunGao <gaolinkun123@gmail.com>
2021-11-13 23:59:20 +1300

    build database

--- myblog/models.py
+++ myblog/models.py
@@ -4,7 +4,22 @@ from django.db import models
 class SiteInfo(models.Model):
     title = models.CharField(null=True,blank=True, max_length=50)
     icon = models.ImageField(upload_to='logo/', null=True, blank=True)
-    
+
 # pip install Pillow
     def __int__(self):
         return self.id
+
+# crouse categories
+class Crouses(models.Model):
+    text = models.CharField(null=True,blank=True, max_length=50)
+    def __str__(self):
+        return self.text
+
+# users
+class UserInfo(models.Model):
+    nickName = models.CharField(null=True,blank=True, max_length=50)
+    icon = models.ImageField(upload_to='user_icon/', null=True, blank=True)
+    belong = models.ForeignKey(Crouses, on_delete=models.SET_NULL, related_name="userinfo_crouses", null=True, blank=True)
+
+    def __str__(self):
+        return self.nickName
